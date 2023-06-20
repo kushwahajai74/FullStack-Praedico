c8bce529525e998ee00ac3d40e16231f9fc0726a
udayan singh <udayans0107@gmail.com>
2023-06-20 17:19:59 +0530

    Add fxn

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,6 +11,8 @@ export const AppWrapper = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [loading, setLoading] = useState(false);
   const [user, setUser] = useState({});
+  const [isAdmin, setAdmin] = useState(false);
+  const [isHead, setHead] = useState(false);
   return (
     <Context.Provider
       value={{
@@ -20,6 +22,10 @@ export const AppWrapper = () => {
         setLoading,
         user,
         setUser,
+        isAdmin,
+        setAdmin,
+        isHead,
+        setHead
       }}
     >
       <App />
